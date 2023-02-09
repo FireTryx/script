ccfd42f32c8af23a09ae72ed45df907bceff085d
FireTryx_YT <96184599+Loxstazz@users.noreply.github.com>
2023-02-09 22:37:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,10 @@ echo "#"
 echo "#######################################################################################"
 echo ""
 echo "* [0] Installer Minecraft Spigot 1.8.8"
-echo "* [1] Quitter"
+echo "* [1] Installer Minecraft Spigot Latest"
+echo "* [2] Installer Minecraft Forge Server 1.12.2"
+echo "* [3] Installer Minecraft Java Vanilla Latest"
+echo "* [4] Quitter"
 
 read -p "Enter Input (0-1): " input
 
@@ -33,14 +36,58 @@ case $input in
     echo ""
     echo "##############################"
     wget https://cdn.getbukkit.org/spigot/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar
-    wget https://raw.githubusercontent.com/Loxstazz/script/main/start.sh
     mv spigot-1.8.8-R0.1-SNAPSHOT-latest.jar server.jar
+    wget https://raw.githubusercontent.com/Loxstazz/script/main/eula.txt
+    wget https://raw.githubusercontent.com/Loxstazz/script/main/start.sh
+    chmod +x start.sh
     echo "##############################################################"
     echo ""
     echo "Installation terminé"
     echo "Vous pouvez modifier le fichier start.sh avec nano start.sh"
     echo ""
     echo "##############################################################";;
+    
     1)
+    apt update
+    apt install default-jre screen -y
+    apt update
+    echo "##############################"
+    echo ""
+    echo "Java à été installé"
+    echo ""
+    echo "##############################"
+    wget https://download.getbukkit.org/spigot/spigot-1.19.3.jar
+    mv spigot-1.19.3.jar server.jar
+    wget https://raw.githubusercontent.com/Loxstazz/script/main/eula.txt
+    wget https://raw.githubusercontent.com/Loxstazz/script/main/start.sh
+    chmod +x start.sh
+    echo "##############################################################"
+    echo ""
+    echo "Installation terminé"
+    echo "Vous pouvez modifier le fichier start.sh avec nano start.sh"
+    echo ""
+    echo "##############################################################";;
+    
+    2)
+    apt update
+    apt install default-jre screen -y
+    apt update
+    echo "##############################"
+    echo ""
+    echo "Java à été installé"
+    echo ""
+    echo "##############################"
+    wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.12.2-14.23.5.2860/forge-1.12.2-14.23.5.2860-installer.jar
+    mv forge-1.12.2-14.23.5.2860-installer.jar server.jar
+    wget https://raw.githubusercontent.com/Loxstazz/script/main/start.sh
+    chmod +x start.sh
+    java -jar forge-1.12.2-14.23.5.2860-installer.jar --installServer
+    echo "##############################################################"
+    echo ""
+    echo "Installation terminé"
+    echo "Vous pouvez modifier le fichier start.sh avec nano start.sh"
+    echo ""
+    echo "##############################################################";;
+    4)
     echo "Fermeture du Script" && exit;;
 esac
