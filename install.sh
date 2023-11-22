8596b1a9bf3e84fc7fac1a0c3de683729a10c403
FireTryx_YT <96184599+FireTryx@users.noreply.github.com>
2023-11-22 17:07:48 +0100

    Adding PocketMine-MP Download option

--- install.sh
+++ install.sh
@@ -19,7 +19,8 @@ echo "* [0] Installer Minecraft Spigot 1.8.8"
 echo "* [1] Installer Minecraft Spigot Latest"
 echo "* [2] Installer Minecraft Forge Server 1.12.2 (Ne marche pas :("
 echo "* [3] Installer Minecraft Java Vanilla Latest"
-echo "* [4] Quitter"
+echo "* [4] Installer PocketMine-MP 1.20.40 (Latest version: PMMP 5.8.2)"
+echo "* [5] Quitter"
 
 read -p "Enter Input (0-1): " input
 
@@ -114,7 +115,54 @@ case $input in
     echo ""
     echo "##############################################################"
     rm install.sh;;
-    
+
     4)
+    apt update
+    apt install openjdk-17-jre screen -y
+    apt update
+    echo "##############################"
+    echo ""
+    echo "Java à été installé"
+    echo ""
+    echo "##############################"
+    cd /tmp/
+    wget https://github.com/pmmp/PocketMine-MP/releases/download/5.8.2/PocketMine-MP.phar
+    echo "##############################################################"
+    echo ""
+    echo "Création du dossier d'intallation..."
+    echo "/home/server"
+    echo ""
+    echo "##############################################################"
+    mkdir /home/server
+    cd /home/server
+    mv /tmp/PocketMine-MP.phar /home/server/server.sh
+    wget https://raw.githubusercontent.com/FireTryx/script/main/server_start.sh
+    chmod +x server_start.sh
+    echo "##############################################################"
+    echo ""
+    echo "Installation de PHP..."
+    echo "Version: PHP Latest Binaries for PocketMine-MP (8.1)"
+    echo ""
+    echo "##############################################################"
+    cd /home/server
+    wget https://github.com/pmmp/PHP-Binaries/releases/download/php-8.1-latest/PHP-Linux-x86_64-PM5.tar.gz
+    mv /home/server/php-8.1-latest/PHP-Linux-x86_64-PM5.tar.gz /home/server/php-bin.tar.gz
+    echo "##############################################################"
+    echo ""
+    echo "Dézippage de PHP"
+    echo "Dossier: /bin/"
+    echo ""
+    echo "##############################################################"
+    tar -xzvf php-bin.tar.gz
+    rm php-bin.tar.gz
+    echo "##############################################################"
+    echo ""
+    echo "Installation terminé"
+    echo "Utiliser ./server_start.sh pour démarrer le serveur"
+    echo ""
+    echo "##############################################################"
+    rrm install.sh;;
+    
+    5)
     echo "Fermeture du Script" && exit;;
 esac
